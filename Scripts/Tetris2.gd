de971f7a5dbbf0108fd1fad0641af741700e21a1
Eurientis <sinbrive@users.noreply.github.com>
2023-07-03 21:38:36 +0200

    minor improvements

--- Scripts/Tetris2.gd
+++ Scripts/Tetris2.gd
@@ -4,7 +4,7 @@ onready var tile=preload("res://Tetris/Tile.tscn")
 onready var shape=preload("res://Tetris/Tetromino.tscn")
 onready var raycasts=preload("res://Tetris/Raycasts.tscn")
 onready var tiles_2=preload("res://Tetris/Tiles_2.tscn")
-onready var tiles_set=preload("res://Tetris/Tiles_2_set.tscn")
+onready var tiles_2_set=preload("res://Tetris/Tiles_2_set.tscn")
 
 const GRID=32
 
@@ -23,59 +23,37 @@ var inputs={
 	'ui_down_timer': Vector2.DOWN
 	}
 
-func _ready():
-	randomize()
-	var i=randi() % global.tetros_set.size()
-	index=i
-	current_shape = get_shape(global.tetros_set[index], 0)
-	$ShapeArea.add_child(current_shape)
-	lines = tiles_set.instance()
-	$ShapeArea.add_child(lines)
-	$Timer.start()
-	
-	
-func _physics_process(delta):
-	pass
-
-
-func get_shape(num, _rot):
-	var coords=[]
-	var s = shape.instance()
-	var tetros= num[_rot]
-	for coord in tetros:
-		var b = tile.instance()
-		b.modulate = global.colors[global.tetros_set.find(num, 0)]
-		b.position=Vector2(coord.x*GRID, coord.y*GRID)  
-		s.add_child(b)
-		coords.append(Vector2(coord.x*GRID, coord.y*GRID))
-	s.position=Vector2(GRID/2, GRID/2)  # centrered
-	
-	var ray = raycasts.instance()
-	ray.get_node("left").position=coords[0]
-	ray.get_node("right").position=coords[1]
-	ray.get_node("up").position=coords[2]
-	ray.get_node("down").position=coords[3]
-	
-	s.add_child(ray)
-
-	return s
-	
-func is_overlapped_at_floor(): # not used
-	pass
-
 func _unhandled_input(event):
 	for dir in inputs.keys():
 		if event.is_action_pressed(dir):
 			move(dir)
-			
+
+func _ready():
+	randomize()
+	index=randi() % global.tetros_set.size()
+	current_shape = get_shape(global.tetros_set[index], 0)
+	$ShapeArea.add_child(current_shape)
+	lines = tiles_2_set.instance()
+	$ShapeArea.add_child(lines)
+	$Timer.start()
+	
+func _physics_process(delta):
+	pass
+
+func _on_Timer_timeout():
+	move('ui_down_timer')
+
+func _on_nextShapeTimer_timeout():
+	move('ui_down_timer')
+
 func move(dir):
-	# rotation
 	
 	if dir=='ui_down':
 		$fallTimer.wait_time=0.01
 		$fallTimer.start()
 		return
 	
+	# rotation
 	if dir=='ui_up':
 		if !can_rotate(current_shape, rot, index): 
 			return
@@ -106,7 +84,6 @@ func move(dir):
 			current_shape.position += vect_pos
 			move_squares_to_static()
 			
-#			$ShapeArea.remove_child(current_shape) ## not to be done
 			$fallTimer.stop()
 			check_lines_full()
 			
@@ -115,10 +92,30 @@ func move(dir):
 			current_shape = get_shape(global.tetros_set[i], 0)
 			$ShapeArea.add_child(current_shape)
 			return
-			break
 			
 	current_shape.position += vect_pos
 			
+			
+func get_shape(num, _rot):
+	var coords=[]
+	var s = shape.instance()
+	var tetros= num[_rot]
+	for coord in tetros:
+		var b = tile.instance()
+		b.modulate = global.colors[global.tetros_set.find(num, 0)]
+		b.position=Vector2(coord.x*GRID, coord.y*GRID)  
+		s.add_child(b)
+		coords.append(Vector2(coord.x*GRID, coord.y*GRID))
+	s.position=Vector2(GRID/2, GRID/2)  # centrered
+	
+	var ray = raycasts.instance()
+	ray.get_node("left").position=coords[0]
+	ray.get_node("right").position=coords[1]
+	ray.get_node("up").position=coords[2]
+	ray.get_node("down").position=coords[3]
+	
+	s.add_child(ray)
+	return s
 
 func can_rotate(sh, _rot, _index):  # by reference !
 	var s = shape.instance()
@@ -141,11 +138,9 @@ func can_rotate(sh, _rot, _index):  # by reference !
 	return true
 	
 func is_out_screen(ch):
-	
 	return (ch.global_position.x > 9.5*GRID or ch.global_position.x < 1.5*GRID or
 				 ch.global_position.y < 1.5*GRID or  ch.global_position.y > 20.5*GRID )
 	
-
 func move_squares_to_static():
 	for ch in current_shape.get_children():
 		if ch.name == "Raycasts": 
@@ -157,7 +152,6 @@ func move_squares_to_static():
 			
 	$ShapeArea.remove_child(current_shape)
 	current_shape.queue_free()
-
 
 func check_lines_full() -> void:
 	var count = 0
@@ -172,7 +166,6 @@ func check_lines_full() -> void:
 			remove_line(row)
 		count = 0
 		
-
 func remove_line(row):
 	var updated_lines = []
 	for ln in lines.get_children():
@@ -184,10 +177,3 @@ func remove_line(row):
 		if ln.global_position.y  < row:
 			ln.global_position.y += GRID
 
-
-func _on_Timer_timeout():
-	move('ui_down_timer')
-
-
-func _on_nextShapeTimer_timeout():
-	move('ui_down_timer')
